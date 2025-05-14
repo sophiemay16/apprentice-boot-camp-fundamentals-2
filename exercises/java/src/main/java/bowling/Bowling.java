c2f5ff185816c0afe4515651b7a081ccb3cffcdd
Sophie Yates <sophie.yates@autotrader.co.uk>
2025-05-14 14:07:15 +0100

    Added test to score game with a single spare and refactored method to make this pass

--- exercises/java/src/main/java/bowling/Bowling.java
+++ exercises/java/src/main/java/bowling/Bowling.java
@@ -4,8 +4,12 @@ public class Bowling {
 
     public int bowl(int[] rolls) {
         int score = 0;
-        for (int roll: rolls) {
-            score += roll;
+        for (int i=0; i<rolls.length; i+=2) {
+            int frameTotal = rolls[i] + rolls[i+1];
+            score += frameTotal;
+            if (frameTotal == 10) {
+                score += rolls[i+2];
+            }
         }
         return score;
     }
--- exercises/java/src/test/java/bowling/BowlingTest.java
+++ exercises/java/src/test/java/bowling/BowlingTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class BowlingTest {
 
     @Test
-    public void gutterGame() {
+    public void scoreGutterGame() {
         // given
         Bowling bowling = new Bowling();
+        int[] rolls = new int[]{0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0};
 
         // when
-        int[] rolls = new int[]{0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0};
         int actualScore = bowling.bowl(rolls);
 
         // then
@@ -24,13 +24,27 @@ public class BowlingTest {
     public void scoreGame() {
         // given
         Bowling bowling = new Bowling();
+        int[] rolls = new int[]{1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1};
 
         // when
-        int[] rolls = new int[]{1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1};
         int actualScore = bowling.bowl(rolls);
 
         // then
         int expectedScore = 20;
+        assertEquals(expectedScore, actualScore);
+    }
+
+    @Test
+    public void scoreGameWithSingleSpare() {
+        // given
+        Bowling bowling = new Bowling();
+        int[] rolls = new int[]{5,5, 1,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0};
+
+        // when
+        int actualScore = bowling.bowl(rolls);
+
+        // then
+        int expectedScore = 12;
         assertEquals(expectedScore, actualScore);
     }
 }
